0987c64bedf2a45ea0bc581efa58ef4635c90985
iniyan2707 <iniyan.cool3@gmail.com>
2020-08-26 23:56:35 +0530

    added ringtones for user selection and modified the cancel alarm(bonus feature)

--- app/src/main/java/com/example/clock/AlertReceiver.java
+++ app/src/main/java/com/example/clock/AlertReceiver.java
@@ -4,24 +4,52 @@ import android.app.Notification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.widget.RadioButton;
 
 import androidx.core.app.NotificationCompat;
 
+import static com.example.clock.MainActivity.ans;
+
 public class AlertReceiver extends BroadcastReceiver {
-    public static boolean ringtoneplaying;
+    public static boolean mediaplaying;
     public static Ringtone r;
+    public static MediaPlayer mediaPlayer;
+    public static boolean received;
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        received=true;
 
+        if(ans==1){
+            mediaPlayer = MediaPlayer.create(context, R.raw.sound1);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+        }
+        else if(ans==2)
+        {
+             mediaPlayer = MediaPlayer.create(context, R.raw.sound2);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+        }
+        else if(ans==3)
+        {
+            mediaPlayer = MediaPlayer.create(context, R.raw.sound3);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+        }
+        else
+        {
             Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-             r = RingtoneManager.getRingtone(context, notification);
+            mediaPlayer = MediaPlayer.create(context, notification);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+        }
 
-            r.play();
-            ringtoneplaying=true;
+
 
         NotificationHelper notificationHelper=new NotificationHelper(context);
         NotificationCompat.Builder nb=notificationHelper.getChannelNotification();
--- app/src/main/java/com/example/clock/MainActivity.java
+++ app/src/main/java/com/example/clock/MainActivity.java
@@ -10,26 +10,39 @@ import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.util.Calendar;
 
-import static com.example.clock.AlertReceiver.r;
-import static com.example.clock.AlertReceiver.ringtoneplaying;
+import static com.example.clock.AlertReceiver.mediaPlayer;
+import static com.example.clock.AlertReceiver.mediaplaying;
+import static com.example.clock.AlertReceiver.received;
 
 
 public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     private TextView textView;
+    private EditText editText;
     private Button buttonSetAlarm;
     private Button buttonCancel;
     private Button btnmainmenu;
+    private RadioGroup rbgroup;
+    private RadioButton rb1;
+    private RadioButton rb3;
+    private RadioButton rb2;
+    public static int ans;
 
 
 
@@ -38,6 +51,12 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Alarm Clock");
+
+        rbgroup=findViewById(R.id.radioGroup);
+        rb1=findViewById(R.id.radioButton1);
+        rb2=findViewById(R.id.radioButton2);
+        rb3=findViewById(R.id.radioButton3);
+        editText=findViewById(R.id.edittext);
 
         textView = findViewById(R.id.textView);
         buttonSetAlarm = findViewById(R.id.set_alarm);
@@ -90,6 +109,8 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
     }
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     private void startAlarm(Calendar c){
+        RadioButton selected= findViewById(rbgroup.getCheckedRadioButtonId());
+        ans= rbgroup.indexOfChild(selected)+1;
         AlarmManager alarmManager=(AlarmManager) getSystemService(Context.ALARM_SERVICE);
         Intent intent=new Intent(this,AlertReceiver.class);
         PendingIntent pendingIntent= PendingIntent.getBroadcast(this,1,intent,0);
@@ -98,20 +119,111 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
             c.add(Calendar.DATE,1);
         }
         alarmManager.setExact(AlarmManager.RTC_WAKEUP,c.getTimeInMillis(),pendingIntent);
+
     }
 
     private void canelAlarm()
     {
+        closeKeyboard();
+
+        if(!received){
             AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
             Intent intent = new Intent(this, AlertReceiver.class);
             PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
             alarmManager.cancel(pendingIntent);
-
-            if(ringtoneplaying){
-                r.stop();
-            }
             textView.setText("Alarm Cancelled");
+        }
 
+
+            else {
+
+
+                String input = editText.getText().toString();
+               Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_WEEK);
+
+                switch (day) {
+                    case Calendar.SUNDAY:
+                        if(input.equalsIgnoreCase("Sunday"))
+                    {alrcancel();}
+                        else{
+                        Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show();
+                        editText.setText("");
+                    }
+
+                        break;
+                    case Calendar.MONDAY:
+                        if(input.equalsIgnoreCase("Monday"))
+                        alrcancel();
+                        else{
+                            Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show();
+                            editText.setText("");
+                        }
+                        break;
+                    case Calendar.TUESDAY:
+                        if(input.equalsIgnoreCase("Tuesday"))
+                        alrcancel();
+                         else{
+                            Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show();
+                            editText.setText("");
+                        }
+                        break;
+                    case Calendar.WEDNESDAY:
+                        if(input.equalsIgnoreCase("Wednesday"))
+                        alrcancel();
+                        else{
+                            Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show();
+                            editText.setText("");
+                        }
+                        break;
+                    case Calendar.THURSDAY:
+                        if(input.equalsIgnoreCase("Thursday"))
+                        alrcancel();
+                        else{
+                            Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show();
+                            editText.setText("");
+                        }
+                        break;
+                    case Calendar.FRIDAY:
+                        if(input.equalsIgnoreCase("Friday"))
+                        alrcancel();
+                         else{
+                        Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show();
+                            editText.setText("");
+                    }
+                    break;
+                    case Calendar.SATURDAY:
+                        if(input.equalsIgnoreCase("Saturday"))
+                        alrcancel();
+                        else{
+                            Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show();
+                            editText.setText("");
+                        }
+                        break;
+                    default:
+                        Toast.makeText(this, "Enter a valid day", Toast.LENGTH_SHORT).show();
+                        editText.setText("");
+                        break;
+                }
+            }
+
+
+
+
+
+    }
+    private void alrcancel()
+    {
+        received=false;
+        mediaPlayer.stop();
+        textView.setText("Alarm Cancelled");
+    }
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
 
     }
 
